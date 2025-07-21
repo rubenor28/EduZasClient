0132d5d6467b98e1538108926f629a65c0f10415
Nyx325 <omar.roman.2828@gmail.com>
2025-07-20 19:20:00 -0600

    tabla logs

--- api/prisma/migrations/20250721011913_init/migration.sql
+++ api/prisma/migrations/20250721011913_init/migration.sql
@@ -1,17 +1,20 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `Log` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `type` ENUM('SYSTEM_ERROR', 'VALIDATION_ERROR', 'USER_ACTION', 'ADMIN_ACTION', 'DATA_MODIFICATION', 'SECURITY_EVENT', 'EXTERNAL_SERVICE') NOT NULL,
+    `message` VARCHAR(191) NOT NULL,
+    `stacktrace` VARCHAR(191) NULL,
+    `metadata` JSON NULL,
+    `user_id` BIGINT UNSIGNED NULL,
 
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `User`;
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `users` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `tuition` VARCHAR(191) NOT NULL,
-    `active` BOOLEAN NOT NULL,
     `first_name` VARCHAR(191) NOT NULL,
     `mid_name` VARCHAR(191) NULL,
     `father_lastname` VARCHAR(191) NOT NULL,
@@ -34,7 +37,7 @@ CREATE TABLE `classes` (
     `class_name` VARCHAR(100) NOT NULL,
     `subject` VARCHAR(100) NOT NULL,
     `section` VARCHAR(100) NOT NULL,
-    `owner_id` INTEGER NULL,
+    `owner_id` BIGINT UNSIGNED NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `modified_at` DATETIME(3) NOT NULL,
 
@@ -43,9 +46,9 @@ CREATE TABLE `classes` (
 
 -- CreateTable
 CREATE TABLE `classes_professors` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `class_id` VARCHAR(20) NULL,
-    `professor_id` INTEGER NULL,
+    `professor_id` BIGINT UNSIGNED NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `modified_at` DATETIME(3) NOT NULL,
 
@@ -54,9 +57,9 @@ CREATE TABLE `classes_professors` (
 
 -- CreateTable
 CREATE TABLE `classes_studens` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `class_id` VARCHAR(20) NULL,
-    `student_id` INTEGER NULL,
+    `student_id` BIGINT UNSIGNED NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `modified_at` DATETIME(3) NOT NULL,
 
@@ -65,9 +68,9 @@ CREATE TABLE `classes_studens` (
 
 -- CreateTable
 CREATE TABLE `agenda_contacts` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `agenda_owner_id` INTEGER NULL,
-    `contact_id` INTEGER NULL,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `agenda_owner_id` BIGINT UNSIGNED NULL,
+    `contact_id` BIGINT UNSIGNED NULL,
     `alias` VARCHAR(40) NOT NULL,
     `tag` VARCHAR(191) NOT NULL,
     `notes` TEXT NULL,
@@ -79,10 +82,10 @@ CREATE TABLE `agenda_contacts` (
 
 -- CreateTable
 CREATE TABLE `class_resources` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(40) NOT NULL,
     `content` JSON NOT NULL,
-    `professor_id` INTEGER NULL,
+    `professor_id` BIGINT UNSIGNED NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `modified_at` DATETIME(3) NOT NULL,
 
@@ -91,9 +94,9 @@ CREATE TABLE `class_resources` (
 
 -- CreateTable
 CREATE TABLE `classes_resources_link` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `classId` VARCHAR(20) NULL,
-    `material_id` INTEGER NULL,
+    `resource_id` BIGINT UNSIGNED NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `modified_at` DATETIME(3) NOT NULL,
 
@@ -102,7 +105,7 @@ CREATE TABLE `classes_resources_link` (
 
 -- CreateTable
 CREATE TABLE `class_tests` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(40) NOT NULL,
     `password` VARCHAR(25) NULL,
     `content` JSON NOT NULL,
@@ -114,14 +117,17 @@ CREATE TABLE `class_tests` (
 
 -- CreateTable
 CREATE TABLE `classes_tests_link` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `class_id` VARCHAR(20) NULL,
-    `test_id` INTEGER NULL,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `classId` VARCHAR(20) NULL,
+    `test_id` BIGINT UNSIGNED NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `modified_at` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Log` ADD CONSTRAINT `Log_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `classes` ADD CONSTRAINT `classes_owner_id_fkey` FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
@@ -151,10 +157,10 @@ ALTER TABLE `class_resources` ADD CONSTRAINT `class_resources_professor_id_fkey`
 ALTER TABLE `classes_resources_link` ADD CONSTRAINT `classes_resources_link_classId_fkey` FOREIGN KEY (`classId`) REFERENCES `classes`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `classes_resources_link` ADD CONSTRAINT `classes_resources_link_material_id_fkey` FOREIGN KEY (`material_id`) REFERENCES `class_resources`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `classes_resources_link` ADD CONSTRAINT `classes_resources_link_resource_id_fkey` FOREIGN KEY (`resource_id`) REFERENCES `class_resources`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `classes_tests_link` ADD CONSTRAINT `classes_tests_link_class_id_fkey` FOREIGN KEY (`class_id`) REFERENCES `classes`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `classes_tests_link` ADD CONSTRAINT `classes_tests_link_classId_fkey` FOREIGN KEY (`classId`) REFERENCES `classes`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `classes_tests_link` ADD CONSTRAINT `classes_tests_link_test_id_fkey` FOREIGN KEY (`test_id`) REFERENCES `class_tests`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
