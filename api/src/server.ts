020314a0c96a604eacfb10cd7bde56e3a2066dcc
Omar Roman <omar.roman.2828@gmail.com>
2025-08-26 11:35:56 -0600

    re-enable cors & not serve static client on backend

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,17 +1,11 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 import { PORT } from "./config";
 
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
-
-if (process.env.BUN_ENV === "production") {
-  const clientDistPath = path.join(__dirname, "..", "..", "client", "dist");
-  app.use(express.static(clientDistPath));
-}
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`\nAPI listening on http://localhost:${PORT}\n`);
