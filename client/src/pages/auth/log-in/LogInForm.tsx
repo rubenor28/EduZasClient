2ed036e38122ef712da5836e249f5dc81d131fe1
Omar Roman <omar.roman.2828@gmail.com>
2025-09-03 21:32:00 -0600

    inicio formulario login

--- client/src/pages/auth/log-in/LogInForm.tsx
+++ client/src/pages/auth/log-in/LogInForm.tsx
@@ -1,10 +1,43 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { userService } from "services";
+import { mapFieldErrorsToFieldMessageMapFromKeys, userService } from "services";
 import { Alert, AlertType, FormInput } from "components";
+import type { UserCredentials } from "entities/users/entities/user.credentials";
+import { authService } from "services/auth.service";
 
-type LogInFormProps = {};
+type InputError = {
+  email?: string;
+  password?: string;
+};
 
-export function LogInForm({}: LogInFormProps) {
-  return <></>
+type FormState =
+  | { state: "idle" | "success" | "unexpected_error" }
+  | ({ state: "input_error" } & InputError);
+
+export function LogInForm() {
+  const navigate = useNavigate();
+
+  const [credentials, setCredentials] = useState<UserCredentials>({
+    email: "",
+    password: "",
+  });
+
+  const [formState, setFormState] = useState<FormState>({ state: "idle" });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    authService.createUser(credentials).then((result) => {
+      const inputErrs = mapFieldErrorsToFieldMessageMapFromKeys<UserCredentials>(
+        result.val,
+        NEW_USER_KEYS,
+      );
+    });
+  };
+
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
 }
