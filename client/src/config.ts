1e5a7a9bc203e9be796a403d51999a25b0a820eb
Omar Roman <omar.roman.2828@gmail.com>
2025-09-09 16:07:10 -0600

    agregar URL de cliente como variable de entorno

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,5 @@
 export const VITE_API_URL = import.meta.env.VITE_API_URL;
+export const VITE_CLIENT_URL = import.meta.env.VITE_CLIENT_URL;
 
 if (VITE_API_URL === undefined) throw Error("VITE_API_URL must be defined in env file");
+if (VITE_CLIENT_URL === undefined) throw Error("VITE_CLIENT_URL must be defined in env file");
