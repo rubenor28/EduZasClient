0426f9a9b99fa9fb665318e7265c83bdd2bdece4
Omar Roman <omar.roman.2828@gmail.com>
2025-09-09 16:34:22 -0600

    remover variable innecesaria VITE_CLIENT_URL

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,4 @@
 export const VITE_API_URL = import.meta.env.VITE_API_URL;
-export const VITE_CLIENT_URL = import.meta.env.VITE_CLIENT_URL;
 
-if (VITE_API_URL === undefined) throw Error("VITE_API_URL must be defined in env file");
-if (VITE_CLIENT_URL === undefined) throw Error("VITE_CLIENT_URL must be defined in env file");
+if (VITE_API_URL === undefined)
+  throw Error("VITE_API_URL must be defined in env file");
