1214fa8860a785ee5316e6ca755d7fcace4238bc
Omar Roman <omar.roman.2828@gmail.com>
2025-09-13 12:33:42 -0600

    comentar app state no usado

--- client/src/state/app.state.ts
+++ client/src/state/app.state.ts
@@ -1,29 +1,29 @@
-import { create } from "zustand";
-import type { User } from "entities/users/entities";
-
-/**
- * Estado global de la aplicación.
- */
-interface AppState {
-  isAuth: () => User | undefined;
-  logout: () => void;
-}
-
-/**
- * Store global de la aplicación usando `zustand`.
- * ```
- */
-export const appState = create<AppState>((set) => ({
-  auth: { isAuth: false },
-
-  logout: async () => {
-    try {
-      await fetch("/auth/logout", {
-        method: "DELETE",
-        credentials: "include",
-      });
-    } catch (err) {
-      console.error("Error al cerrar sesión:", err);
-    }
-  },
-}));
+// import { create } from "zustand";
+// import type { User } from "entities/users/entities";
+// 
+// /**
+//  * Estado global de la aplicación.
+//  */
+// interface AppState {
+//   isAuth: () => User | undefined;
+//   logout: () => void;
+// }
+// 
+// /**
+//  * Store global de la aplicación usando `zustand`.
+//  * ```
+//  */
+// export const appState = create<AppState>((set) => ({
+//   auth: { isAuth: false },
+// 
+//   logout: async () => {
+//     try {
+//       await fetch("/auth/logout", {
+//         method: "DELETE",
+//         credentials: "include",
+//       });
+//     } catch (err) {
+//       console.error("Error al cerrar sesión:", err);
+//     }
+//   },
+// }));
