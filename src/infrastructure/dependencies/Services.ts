f42a3bfadcba35ac4b7c6bed0affb2eca81825bf
Omar Roman <omar.roman.2828@gmail.com>
2025-10-02 08:58:06 -0600

    instancia fetch class service

--- src/infrastructure/dependencies/Services.ts
+++ src/infrastructure/dependencies/Services.ts
@@ -1,5 +1,10 @@
-import { createFetchAuthService } from "@infrastructure-fetch";
 import { API_URL } from "./EnvVars";
-import type { AuthService } from "@application";
+import type { AuthService, ClassService } from "@application";
+
+import {
+  createFetchAuthService,
+  createFetchClassService,
+} from "@infrastructure-fetch";
 
 export const authService: AuthService = createFetchAuthService(API_URL);
+export const classService: ClassService = createFetchClassService(API_URL);
