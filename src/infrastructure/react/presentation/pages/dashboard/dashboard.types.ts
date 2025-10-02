a474639c27f4a2fa610a36a5147211ddad78eeff
Omar Roman <omar.roman.2828@gmail.com>
2025-10-02 08:47:53 -0600

    Asociar evaluacion de rol a tipo usuario y no usuario en general

--- src/infrastructure/react/presentation/pages/dashboard/dashboard.types.ts
+++ src/infrastructure/react/presentation/pages/dashboard/dashboard.types.ts
@@ -1,4 +1,5 @@
 import { mapStringToEnum } from "@application";
+import { UserType } from "@domain";
 
 export const DashboardTabs = {
   SystemClasses: "SystemClasses",
@@ -8,8 +9,17 @@ export const DashboardTabs = {
   Tests: "Tests",
 } as const;
 
-export type DashboardTabs =
-  (typeof DashboardTabs)[keyof typeof DashboardTabs];
+export type DashboardTabs = (typeof DashboardTabs)[keyof typeof DashboardTabs];
 
 export const mapDashboardPage = (key: string) =>
   mapStringToEnum(key, DashboardTabs);
+
+export const defaultTab = (role: UserType) => {
+  const tabs = {
+    [UserType.STUDENT]: DashboardTabs.EnrolledClasses,
+    [UserType.PROFESSOR]: DashboardTabs.MyClasses,
+    [UserType.ADMIN]: DashboardTabs.SystemClasses,
+  };
+
+  return tabs[role] ?? DashboardTabs.EnrolledClasses;
+};
